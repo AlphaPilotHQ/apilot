9ddb19905fefe2430a5d9710792852d1f6c59329
alin <81708155+alinalphapilot@users.noreply.github.com>
2025-03-11 19:48:20 +0800

    重构项目结构：添加cta_strategy模块和strategy示例

---
+++ vnpy/cta_strategy/__init__.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+from typing import Type
+
+from vnpy.trader.app import BaseApp
+from vnpy.trader.constant import Direction
+from vnpy.trader.object import TickData, BarData, TradeData, OrderData
+from vnpy.trader.utility import BarGenerator, ArrayManager
+
+from .base import APP_NAME, StopOrder
+from .engine import CtaEngine
+from .template import CtaTemplate, CtaSignal, TargetPosTemplate
+
+
+
+class CtaStrategyApp(BaseApp):
+    """"""
+
+    app_name: str = APP_NAME
+    app_module: str = __module__
+    app_path: Path = Path(__file__).parent
+    display_name: str = "CTA策略"
+    engine_class: Type[CtaEngine] = CtaEngine
+    widget_name: str = "CtaManager"
+    icon_name: str = str(app_path.joinpath("ui", "cta.ico"))
