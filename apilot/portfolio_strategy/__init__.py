05abe6c29b9f7312bd096609a766b62e8aa25761
alin <81708155+alinalphapilot@users.noreply.github.com>
2025-03-22 19:17:39 +0800

    refactor: remove GUI-related app classes from strategy modules
    - Remove CtaStrategyApp class from cta_strategy module
    - Remove PortfolioStrategyApp class from portfolio_strategy module
    - Remove unnecessary imports for BaseApp
    - Streamline module initialization for script-only usage

--- apilot/portfolio_strategy/__init__.py
+++ apilot/portfolio_strategy/__init__.py
@@ -7,7 +7,6 @@ if sys.version_info >= (3, 8):
 else:
     import importlib_metadata
 
-from apilot.trader.app import BaseApp
 from apilot.trader.constant import Direction
 from apilot.trader.object import TickData, BarData, TradeData, OrderData
 from apilot.trader.utility import BarGenerator, ArrayManager
@@ -21,15 +20,3 @@ try:
     __version__ = importlib_metadata.version("apilot_portfoliostrategy")
 except importlib_metadata.PackageNotFoundError:
     __version__ = "dev"
-
-
-class PortfolioStrategyApp(BaseApp):
-    """"""
-
-    app_name: str = APP_NAME
-    app_module: str = __module__
-    app_path: Path = Path(__file__).parent
-    display_name: str = "组合策略"
-    engine_class: StrategyEngine = StrategyEngine
-    widget_name: str = "PortfolioStrategyManager"
-    icon_name: str = str(app_path.joinpath("ui", "strategy.ico"))
