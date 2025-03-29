6191b0c9bbd6096fdd000787b8daa7e381114235
alin <81708155+alinalphapilot@users.noreply.github.com>
2025-03-29 13:45:53 +0800

    refactor: 修复数据库接口类型标注和异常处理

--- apilot/core/database.py
+++ apilot/core/database.py
@@ -7,7 +7,6 @@
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
 from datetime import datetime
-from typing import Dict, List, Optional, Type
 
 from .constant import Exchange, Interval
 from .object import BarData, TickData
@@ -24,6 +23,7 @@ class BarOverview:
     """
     Overview of bar data stored in database.
     """
+
     symbol: str = ""
     exchange: Exchange = None
     interval: Interval = None
@@ -37,6 +37,7 @@ class TickOverview:
     """
     Overview of tick data stored in database.
     """
+
     symbol: str = ""
     exchange: Exchange = None
     count: int = 0
@@ -56,54 +57,46 @@ class BaseDatabase(ABC):
         exchange: Exchange,
         interval: Interval,
         start: datetime,
-        end: datetime
-    ) -> List[BarData]:
+        end: datetime,
+    ) -> list[BarData]:
         pass
 
     @abstractmethod
     def load_tick_data(
-        self,
-        symbol: str,
-        exchange: Exchange,
-        start: datetime,
-        end: datetime
-    ) -> List[TickData]:
+        self, symbol: str, exchange: Exchange, start: datetime, end: datetime
+    ) -> list[TickData]:
         pass
 
     @abstractmethod
     def delete_bar_data(
-        self,
-        symbol: str,
-        exchange: Exchange,
-        interval: Interval
+        self, symbol: str, exchange: Exchange, interval: Interval
     ) -> int:
         pass
 
     @abstractmethod
-    def delete_tick_data(
-        self,
-        symbol: str,
-        exchange: Exchange
-    ) -> int:
+    def delete_tick_data(self, symbol: str, exchange: Exchange) -> int:
         pass
 
     @abstractmethod
-    def get_bar_overview(self) -> List[BarOverview]:
+    def get_bar_overview(self) -> list[BarOverview]:
         pass
 
     @abstractmethod
-    def get_tick_overview(self) -> List[TickOverview]:
+    def get_tick_overview(self) -> list[TickOverview]:
         pass
+
 
 # 全局数据库实例
-database: Optional[BaseDatabase] = None
+database: BaseDatabase | None = None
 
 # 数据库插件注册表
-_DATABASE_REGISTRY: Dict[str, Type[BaseDatabase]] = {}
+_DATABASE_REGISTRY: dict[str, type[BaseDatabase]] = {}
+
 
 def register_database(name: str, database_class: type) -> None:
     """注册自定义数据库实现"""
     _DATABASE_REGISTRY[name] = database_class
+
 
 def use_database(name: str, **kwargs) -> BaseDatabase:
     """使用指定的数据库实现"""
@@ -112,6 +105,7 @@ def use_database(name: str, **kwargs) -> BaseDatabase:
         return database_class(**kwargs)
     else:
         raise ValueError(f"未找到数据库实现: {name}")
+
 
 def get_database() -> BaseDatabase:
     """
@@ -124,21 +118,14 @@ def get_database() -> BaseDatabase:
 
     # 从设置中读取数据库类型
     database_name = DATABASE_CONFIG.get("name", "")
+    database_params = DATABASE_CONFIG.get("params", {})
 
-    # 提取对应数据库类型的参数
-    database_params = {}
-    if database_name == "csv":
-        database_params["data_path"] = DATABASE_CONFIG.get("data_path", "data")
-
-    try:
-        database = use_database(database_name, **database_params)
-        return database
-    except Exception:
-        # 默认使用已注册的数据库,或者抛出异常
-        if _DATABASE_REGISTRY:
-            first_db_name = next(iter(_DATABASE_REGISTRY))
-            database_class = _DATABASE_REGISTRY[first_db_name]
-            database = database_class()
+    # 如果配置了数据库,则初始化
+    if database_name:
+        try:
+            database = use_database(database_name, **database_params)
             return database
-        else:
-            raise ValueError("没有可用的数据库实现")
+        except Exception as err:
+            raise ValueError(f"初始化数据库 {database_name} 失败") from err
+    else:
+        raise ValueError("没有可用的数据库实现") from None
